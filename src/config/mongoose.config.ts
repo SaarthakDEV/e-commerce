4c5d65816b8d5982385c9fa02e0699eec90d5cc1
SaarthakDEV <dev.saaarthak@gmail.com>
2025-10-22 13:17:22 +0530

    fix: Improve MongoDB connection handling and add error checking for connection URL

--- src/config/mongoose.config.ts
+++ src/config/mongoose.config.ts
@@ -1,9 +1,25 @@
 import mongoose from "mongoose";
 
-export const connectToMongo = async () => {
-    try{
-        await mongoose.connect(process.env.DB_CONNECTION_URL as string);
-    }catch(err){
+const MONGO_URI = process.env.DB_CONNECTION_URL! as string;
 
-    }
-}
+if (!MONGO_URI)
+  throw new Error("Missing connection url in environment variables");
+
+let isConnected = false;
+
+export const connectToMongo = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(MONGO_URI, {
+      bufferCommands: false, // Important for serverless
+    });
+    isConnected = true;
+    console.log("MongoDB connected:", db.connection.name);
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    throw new Error("Failed to connect to MongoDB");
+  }
+};
